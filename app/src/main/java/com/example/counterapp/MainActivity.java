563a0c6b9a8220f194dedb6f01f79aadd4d2df11
vinayak <itssvinayak@gmail.com>
2022-11-06 23:42:18 +0530

    count up and down button

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -1,29 +1,42 @@
 package com.example.counterapp;
 
+import static com.example.counterapp.R.id.countBtnUp;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int count = 0;
+    Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button countBtn = findViewById(R.id.countBtn);
+        ImageButton countBtnUp, countBtnDown;
+        countBtnUp = findViewById(R.id.countBtnUp);
+        countBtnDown = findViewById(R.id.countBtnDown);
         Button resetBtn = findViewById(R.id.resetBtn);
         TextView counter = findViewById(R.id.textView);
-        countBtn.setOnClickListener(new View.OnClickListener() {
+        countBtnUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ++count;
                 counter.setText(new String(String.valueOf(count)));
-                Toast.makeText(MainActivity.this, "count increased by one", Toast.LENGTH_SHORT).show();
-
+            }
+        });
+        countBtnDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                --count;
+                counter.setText(new String(String.valueOf(count)));
             }
         });
         resetBtn.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +44,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 count = 0;
                 counter.setText(new String(String.valueOf(count)));
-                Toast.makeText(MainActivity.this, "count reset", Toast.LENGTH_SHORT).show();
+                toast = Toast.makeText(MainActivity.this, "count reset", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
             }
         });
     }
